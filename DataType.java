f7a74694679fd9188680f6eb636d6fc80bb5f30a
harinimn19 <harinibalachandhar19@gmail.com>
2025-07-20 08:38:23 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int amount=30;
+		float amount2=2.03f;
+		char letter='A';
+		String name="naga";
+		
+		System.out.println(amount);
+		System.out.println(amount2);
+		System.out.println(name);
+		System.out.println(letter);
+		
+	}
+
+}
